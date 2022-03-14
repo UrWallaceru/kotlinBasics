f367f2e4553bd80c34dfa27e990c10f34b2375e3
UrWallace <l18121495@morelia.tecnm.mx>
2022-03-14 00:36:17 -0600

    Tercera tarea, salu3 profe jsjsjsjs

--- app/src/main/java/com/example/kotlinbasics/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinbasics
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -10,8 +12,50 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.kotlinbasics.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    val constante: String = "Una constante"
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_main)
+
+        val a: Int = 1
+        //a = 5
+        /*
+        *
+        * comentario de varias líneas
+        * NO ES NECESARIO PONER ;
+        * */
+        var b: Int = 2
+        b = 3
+        // buscar esta cosa: Log.i()
+        Log.i("Basics","valor de b= $b" )
+        Log.i("Basics", constante )
+
+        ejecutar()
+
+    }
+
+    fun ejecutar(){
+        val x = 1
+        when(x){
+            1 -> Log.i("Basics","es un uno")
+            2 -> {
+                var algo = "asdas"
+                var otro = "qweqw"
+                Log.i("Basics",algo+otro)
+            }
+            else -> Log.i("Basics","tas mal mijo")
+        }
+    }
+
+
+    fun ejemplo(a: Int): Boolean{
+        return true
+    }
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
